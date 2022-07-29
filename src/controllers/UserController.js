b9ff02c9f5403a4fdc3c9c0d69f69f9aa1a0d926
Matheus Martins <matheusmmartins8@gmail.com>
2022-07-29 16:22:38 -0300

    Rota e método de update

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -25,8 +25,40 @@ class UserController {
   }
 
   // Show
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch(e) {
+      return res.json(null);
+    }
+  }
 
   // Update
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          error: ['Usuário inexistente'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+
+      return res.json(novosDados);
+
+    } catch(e) {
+      return res.json(null);
+    }
+  }
 
   // Delete
 }
