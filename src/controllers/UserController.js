03f6930c6514418d7bc31f653483a02e40ac413c
Matheus Martins <matheusmmartins8@gmail.com>
2022-07-29 17:48:12 -0300

    Conclusão do CRUD de usuários

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,7 +6,7 @@ class UserController {
       const novoUser = await User.create(req.body);
       return res.json(novoUser);
 
-    } catch(e) {
+    } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
@@ -19,7 +19,7 @@ class UserController {
     try {
       const users = await User.findAll();
       return res.json(users);
-    } catch(e) {
+    } catch (e) {
       return res.json(null);
     }
   }
@@ -29,7 +29,7 @@ class UserController {
     try {
       const user = await User.findByPk(req.params.id);
       return res.json(user);
-    } catch(e) {
+    } catch (e) {
       return res.json(null);
     }
   }
@@ -55,12 +55,40 @@ class UserController {
 
       return res.json(novosDados);
 
-    } catch(e) {
-      return res.json(null);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
     }
   }
 
   // Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          error: ['Usuário inexistente'],
+        });
+      }
+
+      await user.destroy();
+
+      return res.json(user);
+
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new UserController();
